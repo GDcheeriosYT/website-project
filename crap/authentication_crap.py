355cc7a0649f617a4997e7c9dd06e850649c85a9
GDcheerios <minecreeper0913@gmail.com>
2021-12-02 02:47:50 -0600

    made client_grant async

--- crap/authentication_crap.py
+++ crap/authentication_crap.py
@@ -5,7 +5,7 @@ import Client_Credentials as client
 import requests
 import time
 
-def client_grant_loop():
+async def client_grant_loop():
     global response
     while True:
         response = requests.post(f"https://osu.ppy.sh/oauth/token", headers = {"Accept": "application/json", "Content-Type": "application/json",}, body = {"client_id": client.client_id, "client_secret": f"{client.secret}", "grant_type": "client_credentials", "scope": "public"}).json()
