b683bebacb5d76c2d8f2ca50a9f1f78b489dff02
GDcheerios <minecreeper0913@gmail.com>
2021-12-14 18:49:22 -0600

    disable random restarts

--- main.py
+++ main.py
@@ -428,6 +428,6 @@ if __name__ == "__main__":  # Makes sure this is the main process
   app.run( # Starts the site
     host='0.0.0.0',  # Establishes the host, required for repl to detect the site
     port=80,# Randomly select the port the machine hosts on.
-    debug=True,
+    debug=False,
     #ssl_context='adhoc'
   )
