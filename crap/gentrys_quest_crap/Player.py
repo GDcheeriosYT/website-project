1cb5685666a8d4ccad0da1249c5242951fc27ae0
GDcheerios <minecreeper0913@gmail.com>
2024-02-09 00:12:31 -0600

    updated ranking section

--- crap/gentrys_quest_crap/Player.py
+++ crap/gentrys_quest_crap/Player.py
@@ -12,6 +12,7 @@ class Player:
         self.account_name = account_name
         self.id = id
         self.data = data
+        self.color = "#00000000"
         self.power_level = PowerLevel()
         self.ranking = Ranking()
 
@@ -21,23 +22,7 @@ class Player:
         self.power_level.unweighted,self.power_level.weighted = gps_stuff['rating']['unweighted'], gps_stuff['rating']['weighted']
         self.ranking.rank, self.ranking.tier = gps_stuff['ranking']['rank'], gps_stuff['ranking']['tier']
         print(f"{self.account_name} power level just updated!\n{old_pl} -> {self.power_level.unweighted} {self.ranking}")
-        if self.ranking.rank == 'copper':
-            self.color = "red"
-        elif self.ranking.rank == 'bronze':
-            self.color = "brown"
-        elif self.ranking.rank == 'silver':
-            self.color = "gray"
-        elif self.ranking.rank == 'gold':
-            self.color = "gold"
-        elif self.ranking.rank == 'platinum':
-            self.color = "blue"
-        elif self.ranking.rank == 'diamond':
-            self.color = "cyan"
-        elif self.ranking.rank == 'gentry warrior':
-            self.color = "lime"
-        else:
-            self.color = "#00000000"
-            self.ranking = Ranking()
+        self.color = rater.rating_colors[self.ranking.rank]
 
     def jsonify(self):
         json_thing = {
