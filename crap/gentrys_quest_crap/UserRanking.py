ff984f7c3dcf0ce7e3702bc894c5de5a0a5c21b8
GDcheerios <minecreeper0913@gmail.com>
2024-10-27 08:51:43 -0500

    added UserRanking

--- crap/gentrys_quest_crap/UserRanking.py
+++ crap/gentrys_quest_crap/UserRanking.py
@@ -2,16 +2,19 @@ from crap.PSQLConnection import PSQLConnection as DB
 
 
 class UserRanking:
-    id = 0
-    weighted = 0
-    unweighted = 0
-    rank = "unranked"
-    tier = "I"
+    id = None
+    weighted = None
+    unweighted = None
+    rank = None
+    tier = None
 
     def __init__(self, id):
         result = DB.get("SELECT c_weighted, c_unweighted, c_rank, c_tier FROM rankings where id = %s;", params=(id,))
         self.id = id,
-        self.weighted = result[0]
-        self.unweighted = result[1]
-        self.rank = result[2]
-        self.tier = result[3]
+        try:
+            self.weighted = result[0]
+            self.unweighted = result[1]
+            self.rank = result[2]
+            self.tier = result[3]
+        except TypeError:
+            pass
